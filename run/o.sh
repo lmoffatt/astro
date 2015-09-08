a03567329a78a21e870f90b543e32629f31a11c7
Luciano Moffatt <lmoffatt@gmail.com>
2015-09-08 10:22:52 -0300

    script para qsubcorr

--- run/o.sh
+++ run/o.sh
@@ -7,8 +7,7 @@
 #$ -l h_rt=1:00:00 # Setea 1 horas de wall clock time
 #$ -V  # Exporta las variables de entorno
 #$ -N test  # El nombre del job
-#$ -d /home/lmoffatt.inquimae/Astro/Astro/runs   # Cambia al directorio runs
-
+#$ -cwd # Cambia al directorio actual
 
 # Comando para correr el programa, tal cual lo llamaríamos desde la línea de comandos
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/lmoffatt.inquimae/opt/gcc-5.2.0/lib64/
