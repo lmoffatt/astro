12a359ef90e0b194e6629ec740ec5756ea507dec
Luciano Moffatt <lmoffatt@gmail.com>
2015-09-20 22:00:29 -0300

    jobs name

--- run/runModelNew.sh
+++ run/runModelNew.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
 
+cd /home/lmoffatt.inquimae/Astro/Astro/run/p_112
+qsub o_112
+
+cd /home/lmoffatt.inquimae/Astro/Astro/run/p_112_22
+qsub o_112_22
+
 cd /home/lmoffatt.inquimae/Astro/Astro/run/p_112_22_31
 qsub o_112_22_31
+
+
+cd /home/lmoffatt.inquimae/Astro/Astro/run/p_113
+qsub o_113
 
 cd /home/lmoffatt.inquimae/Astro/Astro/run/p_113_42
 qsub o_113_42
 
+cd /home/lmoffatt.inquimae/Astro/Astro/run/p_114
+qsub o_114
+
+cd /home/lmoffatt.inquimae/Astro/Astro/run/p_114_24
+qsub o_114_24
+
+
+cd /home/lmoffatt.inquimae/Astro/Astro/run/p_114_24_32_44
+qsub o_114_24_32_44
+
+cd /home/lmoffatt.inquimae/Astro/Astro/run/p_114_24_44
+qsub o_114_24_44
+
+cd /home/lmoffatt.inquimae/Astro/Astro/run/p_115
+qsub o_115
+
+cd /home/lmoffatt.inquimae/Astro/Astro/run/p_115_22
+qsub o_115_22
 
 cd /home/lmoffatt.inquimae/Astro/Astro/run/p_115_25
 qsub o_115_25
-
-cd /home/lmoffatt.inquimae/Astro/Astro/run/p_125
-qsub o_125
 
 
