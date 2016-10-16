29b36a6a18db8e37b29b0df5822481b518263c5c
Luciano Moffatt <lmoffatt@gmail.com>
2016-10-15 21:33:23 -0300

    qsub

--- run/runModelNew.sh
+++ run/runModelNew.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-cd /home/lmoffatt.inquimae/Astro/Astro/run/p_112_51
-qsub o_112_51
+cd /home/lmoffatt.inquimae/Astro/Astro/run/p_000
+qsub o_000
+
+cd /home/lmoffatt.inquimae/Astro/Astro/run/p_100
+qsub o_100
 
 cd /home/lmoffatt.inquimae/Astro/Astro/run/p_112_52
 qsub o_112_52
 
+cd /home/lmoffatt.inquimae/Astro/Astro/run/p_013_51
+qsub o_013_51
 
-cd /home/lmoffatt.inquimae/Astro/Astro/run/p_151
-qsub o_151
 
-cd /home/lmoffatt.inquimae/Astro/Astro/run/p_152
-qsub o_152
+
