03e9633ea151e633d05142447de0aeec3493c540
Luciano Moffatt <lmoffatt@gmail.com>
2017-09-14 17:27:54 -0300

    preparado para jobs en paralelo 2

---
+++ run/runParallel_0.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /home/lmoffatt.inquimae/Astro/Astro/run/m01
+qsub p_m01
+
+cd /home/lmoffatt.inquimae/Astro/Astro/run/m10
+qsub p_m10
+
+cd /home/lmoffatt.inquimae/Astro/Astro/run/m101
+qsub p_m101
+
