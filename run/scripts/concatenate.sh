194c87c5b58134c7cd4b2a5fd37ad9b0f973a10f
Luciano Moffatt <lmoffatt@gmail.com>
2017-10-25 12:17:00 -0300

    concatenate

--- run/scripts/concatenate.sh
+++ run/scripts/concatenate.sh
@@ -6,7 +6,7 @@ cd $1
 a=($ls *par*.000*)
 cp $a par_header
 
-grep $model_[0-9]*[[:space:]][0-9]*[[:space:]][-.0-9]*[[:space:]][0-9]**[[:space:]][0-9]*[[:space:]][-.0-9]*[[:space:]][1]**[[:space:]][0-9]*[[:space:]][-.0-9]*  *par.* -h >temporaryyy
+grep $model_[0-9]*[[:space:]][0-9]*[[:space:]][-.0-9]*[[:space:]][0-9]*[[:space:]][0-9]*[[:space:]][-.0-9]*[[:space:]][1][[:space:]][0-9]*[[:space:]][-.0-9]*  *par.* -h >temporaryyy
 
 cat par_header temporaryyy>../beta_par_$1.txt
 rm temporaryyy
@@ -14,6 +14,23 @@ cd ..
 echo $1 parameters beta=1
 echo $(ls beta_par_$1.txt -lh)
 }
+
+
+extract_par_eq_beta_1(){
+cd $1
+a=($ls *par*.000*)
+cp $a par_header
+
+grep $model_[0-9]*[[:space:]][0-9]*[[:space:]][-.0-9]*[[:space:]][0-9]*[[:space:]][5-9][0-9][0-9][[:space:]][-.0-9]*[[:space:]][1][[:space:]][0-9]*[[:space:]][-.0-9]*  *par.* -h >temporaryyy
+
+cat par_header temporaryyy>../beta_par_$1.txt
+rm temporaryyy
+cd ..
+echo $1 parameters beta=1
+echo $(ls beta_par_$1.txt -lh)
+}
+
+
 extract_all(){
 a=($ls ./m*/*$1.*.000*)
 cp $a $1_header
@@ -33,12 +50,23 @@ echo $(ls total_eq_$1.txt -lh)
 
 }
 
+extract_beta_eq_1(){
+a=($ls ./m*/*$1*.000*)
+cp $a $1_header
+grep $model_[0-9]*[[:space:]][0-9]*[[:space:]][-.0-9]*[[:space:]][0-9]**[[:space:]][5-9][0-9][0-9][[:space:]][-.0-9]*[[:space:]][1][[:space:]][0-9]*[[:space:]][-.0-9]*  ./m*/*$1.* -h >temporaryyy
+
+
+
+cat $1_header temporaryyy>beta_eq_$1_total.txt
+rm temporaryyy
+echo beta=1  $1
+echo $(ls beta_eq_$1_total.txt -lh)
+
+}
 extract_beta_1(){
 a=($ls ./m*/*$1*.000*)
-
 cp $a $1_header
-
-grep $model_[0-9]*[[:space:]][0-9]*[[:space:]][-.0-9]*[[:space:]][0-9]**[[:space:]][0-9]*[[:space:]][-.0-9]*[[:space:]][1]**[[:space:]][0-9]*[[:space:]][-.0-9]*  ./m*/*$1.* -h >temporaryyy
+grep $model_[0-9]*[[:space:]][0-9]*[[:space:]][-.0-9]*[[:space:]][0-9]*[[:space:]][0-9]*[[:space:]][-.0-9]*[[:space:]][1][[:space:]][0-9]*[[:space:]][-.0-9]*  ./m*/*$1.* -h >temporaryyy
 
 cat $1_header temporaryyy>beta_$1_total.txt
 rm temporaryyy
@@ -47,19 +75,18 @@ echo $(ls beta_$1_total.txt -lh)
 
 }
 
+
 extract_all logL
 extract_beta_1 fit
-extract_beta_1 sim
-extract_eq fit
-extract_eq sim
-extract_par_beta_1 m01
-extract_par_beta_1 m02
-extract_par_beta_1 m03
-extract_par_beta_1 m10
-extract_par_beta_1 m101
-extract_par_beta_1 m11
-extract_par_beta_1 m12
-extract_par_beta_1 m13
+extract_beta_eq_1 sim
+extract_par_eq_beta_1 m01
+extract_par_eq_beta_1 m02
+extract_par_eq_beta_1 m03
+extract_par_eq_beta_1 m10
+extract_par_eq_beta_1 m101
+extract_par_eq_beta_1 m11
+extract_par_eq_beta_1 m12
+extract_par_eq_beta_1 m13
 
 
 
