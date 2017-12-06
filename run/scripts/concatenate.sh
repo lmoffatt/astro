eda7ff4c9710d3da36fc2290a4344902c15bdbd9
Luciano Moffatt <lmoffatt@gmail.com>
2017-12-06 17:20:45 -0300

    concatenate

--- run/scripts/concatenate.sh
+++ run/scripts/concatenate.sh
@@ -21,12 +21,12 @@ cd $1
 a=($ls *par*.000*)
 cp $a par_header
 
-grep "$model_[0-9]*[[:space:]][0-9]*[[:space:]][-.0-9]*[[:space:]][0-9]*[[:space:]][1-9][5-9][0-9][0-9][[:space:]][-.0-9]*[[:space:]]\(1\|1[.][01]*e[-]07\|9[.][0-9]*e[-]08\)[[:space:]][0-9]*[[:space:]][-.0-9]*"  *par.*.[1-9][0-9][0-9] -h  >temporaryyy
+grep "$model_[0-9]*[[:space:]][0-9]*[[:space:]][-.0-9]*[[:space:]][0-9]*[[:space:]][1-9][0-9][0-9][0-9][[:space:]][-.0-9]*[[:space:]]\(1\|1[.][01]*e[-]07\|9[.][0-9]*e[-]08\)[[:space:]][0-9]*[[:space:]][-.0-9]*"  *par.*.[1-9][0-9][0-9] -h  >temporaryyy
 
 cat par_header temporaryyy>../beta_par_eq_$1.txt
 rm temporaryyy
 cd ..
-echo $1 parameters nsamples>500 beta=1
+echo $1 parameters nsamples>1000 beta=1
 echo $(ls beta_par_eq_$1.txt -lh)
 }
 
@@ -55,11 +55,11 @@ echo $(ls total_eq_$1.txt -lh)
 extract_beta_eq_1(){
 a=($ls ./m*/*$1*.000*)
 cp $a $1_header
-grep '^model_[0-9]*[[:space:]][0-9]*[[:space:]][-.0-9]*[[:space:]][0-9]**[[:space:]][5-9][0-9][0-9][[:space:]][-.0-9]*[[:space:]][1][[:space:]][0-9]*[[:space:]][-.0-9]*'  ./m*/*$1.* -h >temporaryyy
+grep '^model_[0-9]*[[:space:]][0-9]*[[:space:]][-.0-9]*[[:space:]][0-9]**[[:space:]][1-9][0-9][0-9][0-9][[:space:]][-.0-9]*[[:space:]][1][[:space:]][0-9]*[[:space:]][-.0-9]*'  ./m*/*$1.* -h >temporaryyy
 
 cat $1_header temporaryyy>beta_eq_$1_total.txt
 rm temporaryyy
-echo beta=1 nsamples>500 $1
+echo beta=1 nsamples>1000 $1
 echo $(ls beta_eq_$1_total.txt -lh)
 
 }
@@ -77,16 +77,16 @@ echo $(ls beta_$1_total.txt -lh)
 
 
 extract_all logL
-#extract_beta_1 fit
-#extract_beta_eq_1 sim
-#extract_par_eq_beta_1 m01
+extract_beta_1 fit
+extract_beta_eq_1 sim
+extract_par_eq_beta_1 m01
 #extract_par_eq_beta_1 m02
-#extract_par_eq_beta_1 m03
-#extract_par_eq_beta_1 m10
-#extract_par_eq_beta_1 m101
-#extract_par_eq_beta_1 m11
+extract_par_eq_beta_1 m03
+extract_par_eq_beta_1 m10
+extract_par_eq_beta_1 m101
+extract_par_eq_beta_1 m11
 #extract_par_eq_beta_1 m12
-#extract_par_eq_beta_1 m13
+extract_par_eq_beta_1 m13
 
 
 
